0f46959001e08d66a61102ba28321f56a0908624
hagr7003 <hagr7003@USL1921F722.enterprisenet.org>
2017-05-22 10:23:34 -0400

    Testing commit

--- src/nodecommunication/NodeCommunication.java
+++ src/nodecommunication/NodeCommunication.java
@@ -8,8 +8,8 @@ public class NodeCommunication {
 
 
     public static void main(String[] args) throws IOException {
-        work w = new work();
-        w.getInfo();
+        //work w = new work();
+        //w.getInfo();
         System.out.println("Hello world");
     }
     
